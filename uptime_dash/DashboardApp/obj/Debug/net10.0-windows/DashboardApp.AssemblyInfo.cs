92344a58c5a6681a8774b701ba9f84f0650dd665
Arc NineOhNine <arc@arcnineohnine.com>
2026-02-15 18:18:05 +1100

    cleanup and sync

--- uptime_dash/DashboardApp/obj/Debug/net10.0-windows/DashboardApp.AssemblyInfo.cs
+++ uptime_dash/DashboardApp/obj/Debug/net10.0-windows/DashboardApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashboardApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dbd9690257d07b15a2bb1206076081abb378f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+665b6d4e5f6773d7d27200fb63274020f3b02b93")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashboardApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashboardApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
